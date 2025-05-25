62fb68aeccf55de0cf4f6ba5e3cf9dbcd3c42fc9
valentina mesa <valemf0911@gmail.com>
2025-05-24 21:17:41 -0500

    Propiedad correcta para crearEdificios

--- Assets/Scripts/CrearEdificios.cs
+++ Assets/Scripts/CrearEdificios.cs
@@ -8,21 +8,21 @@ public class ConstruirEdificios : MonoBehaviour
     public void ConstruirCuartel()
     {
         Edificio cuartel = new Cuartel();
-        provincia.edificios.Add(cuartel);
+        provincia.Edificios.Add(cuartel);
         Debug.Log("¡Se construyó un Cuartel!");
     }
 
     public void ConstruirEstablo()
     {
         Edificio establo = new Establo();
-        provincia.edificios.Add(establo);
+        provincia.Edificios.Add(establo);
         Debug.Log("¡Se construyó un Establo!");
     }
 
     public void ConstruirFabrica()
     {
         Edificio fabrica = new Fabrica();
-        provincia.edificios.Add(fabrica);
+        provincia.Edificios.Add(fabrica);
         Debug.Log("¡Se construyó una Fábrica!");
     }
 }
